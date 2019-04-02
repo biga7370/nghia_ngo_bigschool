3b0b705f086a23e599027f175b7e933c23c35e99
Admin <Admin@NGHIANGO-LAPTOP>
2019-04-02 14:42:15 +0700

    Ngay 2/4

---
+++ BigSchool/BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        [Display (Name="Địa điểm")]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        [Display(Name = "Ngày")]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        [Display(Name = "Giờ")]
+        public string Time { get; set; }
+        [Required]
+        [Display(Name = "Danh sách")]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(String.Format("{0} {1}", Date, Time));
+        }
+    }
+}
--- NghiaNgo_BigSchool/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NghiaNgo_BigSchool.MvcApplication" Language="C#" %>
